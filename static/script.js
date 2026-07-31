0397e8ee21043e12f7cbd917eb326121c3188474
sujalchaundkar-afk <sujalchaundkar@gmail.com>
2026-07-31 12:16:50 +0530

    script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,32 @@
-document.querySelector("button").addEventListener("click",function(){
+const imageInput = document.getElementById("imageInput");
+const preview = document.getElementById("preview");
+const report = document.getElementById("report");
+const button = document.getElementById("analyzeBtn");
 
-    alert("AI Analysis Coming Soon...");
+imageInput.addEventListener("change", function () {
+
+    const file = this.files[0];
+
+    if(file){
+
+        preview.src = URL.createObjectURL(file);
+
+        preview.style.display = "block";
+
+    }
+
+});
+
+button.addEventListener("click", function(){
+
+    if(imageInput.files.length===0){
+
+        alert("Please upload a construction site image.");
+
+        return;
+
+    }
+
+    report.innerHTML="Analyzing construction site...";
 
 });
