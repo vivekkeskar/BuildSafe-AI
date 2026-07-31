51079e80c793bb1f9b5b9de9e8073b1c4ca4edfa
sujalchaundkar-afk <sujalchaundkar@gmail.com>
2026-07-31 14:36:33 +0530

    gemini_helper.py

--- gemini_helper.py
+++ gemini_helper.py
@@ -1,1 +1,36 @@
+import google.generativeai as genai
+from PIL import Image
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
+def analyze_image(image_path):
+
+    image = Image.open(image_path)
+
+    prompt = """
+    You are a construction safety inspector.
+
+    Analyze this construction site image.
+
+    Tell me:
+    1. Safety Score out of 100
+    2. PPE violations
+    3. Hazards found
+    4. Risk Level
+    5. Safety recommendations
+
+    Keep the response short and clear.
+    """
+
+    response = model.generate_content([prompt, image])
+
+    return response.text
