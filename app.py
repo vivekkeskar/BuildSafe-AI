88601c8f7c72b5e8ca8e001f2911ee31f1d9f933
sujalchaundkar-afk <sujalchaundkar@gmail.com>
2026-07-31 12:32:56 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+
+    image = request.files["image"]
+
+    if image.filename == "":
+        return "No image selected"
+
+    # AI code will be added later
+    return "Image uploaded successfully."
+
 
 if __name__ == "__main__":
     app.run(debug=True)
